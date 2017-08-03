a013c98599bf445219d3552b0f77e8334a46e1a5
hydejiaqi <hydejiaqi@gmail.com>
2017-08-03 00:17:17 -0700

    Initial commit

---
+++ core/src/com/mygdx/game/screens/PlayScreen.java
@@ -0,0 +1,51 @@
+package com.mygdx.game.screens;
+
+import com.mygdx.game.model.Coord;
+import com.badlogic.gdx.Screen;
+
+
+/**
+ * Created by hydej on 2017/8/3.
+ */
+
+public class PlayScreen implements com.badlogic.gdx.Screen{
+    // Selected squares
+    private Coord selectedeFirstMenpai;
+    private Coord selectedSquareMenpai;
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ core/src/com/mygdx/game/states/State.java
@@ -0,0 +1,8 @@
+package com.mygdx.game.states;
+
+/**
+ * Created by hydej on 2017/8/2.
+ */
+
+public class State {
+}
